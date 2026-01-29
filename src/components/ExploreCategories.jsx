ebc98b6418c3dc2aea23386be5324b35e3c2c391
Aditya Kale <86285188+adityakale3115@users.noreply.github.com>
2026-01-29 15:06:39 +0530

    sample images addded

--- src/components/ExploreCategories.jsx
+++ src/components/ExploreCategories.jsx
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/ExploreCategories.css";
 
+import men1 from '../assets/cat/over.png'
+import men2 from '../assets/cat/shirts.png'
+import men3 from '../assets/cat/cargo.png'
+import men4 from '../assets/cat/jeans.png'
+
+import women1 from '../assets/cat/top.png'
+import women2 from '../assets/cat/kurti.png'
+import women3 from '../assets/cat/high.png'
+import women4 from '../assets/cat/west.png'
+
 const COLLECTION_DATA = {
   Men: [
-    { id: "m1", name: "Oversized Tees", count: "12 Items", image: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?q=80&w=600" },
-    { id: "m2", name: "Premium Shirts", count: "08 Items", image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?q=80&w=600" },
-    { id: "m3", name: "Cargo Pants", count: "15 Items", image: "https://images.unsplash.com/photo-1617137968427-85924c800a22?q=80&w=600" },
-    { id: "m4", name: "Denim Jeans", count: "10 Items", image: "https://images.unsplash.com/photo-1542272604-787c3835535d?q=80&w=600" },
+    { id: "m1", name: "Oversized Tees", count: "12 Items", image: men1 },
+    { id: "m2", name: "Premium Shirts", count: "08 Items", image: men2 },
+    { id: "m3", name: "Cargo Pants", count: "15 Items", image: men3 },
+    { id: "m4", name: "Denim Jeans", count: "10 Items", image: men4 },
   ],
   Women: [
-    { id: "w1", name: "Crop Tops", count: "20 Items", image: "https://images.unsplash.com/photo-1554568218-0f1715e72254?q=80&w=600" },
-    { id: "w2", name: "Elegant Kurtis", count: "14 Items", image: "https://images.unsplash.com/photo-1583391733956-3750e0ff4e8b?q=80&w=600" },
-    { id: "w3", name: "Dresses", count: "09 Items", image: "https://images.unsplash.com/photo-1496747611176-843222e1e57c?q=80&w=600" },
-    { id: "w4", name: "High Rise", count: "11 Items", image: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?q=80&w=600" },
+    { id: "w1", name: "Crop Tops", count: "20 Items", image: women1 },
+    { id: "w2", name: "Elegant Kurtis", count: "14 Items", image: women2 },
+    { id: "w3", name: "Dresses", count: "09 Items", image: women3 },
+    { id: "w4", name: "High Rise", count: "11 Items", image: women4 },
   ],
   Accessories: [
     { id: "a1", name: "Watches", count: "05 Items", image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=600" },
